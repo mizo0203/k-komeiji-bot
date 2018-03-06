f5291edf6bf29fcc416c4de21cb068de6411e688
みぞ@CrazyBeatCoder <mizo0203@mizo0203.com>
2018-03-06 23:57:40 +0900

    cannot find symbol class GitHubAccessTokens. fixes #357 @10m

---
+++ src/main/java/com/mizo0203/komeiji/domain/difine/GitHubAccessTokens.java
@@ -0,0 +1,7 @@
+package com.mizo0203.komeiji.domain.difine;
+
+public class GitHubAccessTokens {
+  // TODO: Set Keys and Access Tokens
+  public static final String MIZO0203_PERSONAL_ACCESS_TOKEN = "";
+  public static final String MUNO0203_PERSONAL_ACCESS_TOKEN = "";
+}
