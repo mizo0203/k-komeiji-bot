4350f5e2d413a6fb2df5b6f81d80841eeb3f1d88
みぞ@CrazyBeatCoder <mizo0203@mizo0203.com>
2018-03-11 11:10:14 +0900

    Revert "[Travis CI] install the Google Cloud SDK."
    This reverts commit 4e9a56cfb78ce5844f6bf39b19266d6529dc8697.

--- setup_tests.sh
+++ setup_tests.sh
@@ -33,21 +33,11 @@ fi
 # http://stackoverflow.com/a/677212/101923
 if ! command -v gcloud  >/dev/null 2>&1; then
 	(
-	# Create an environment variable for the correct distribution
-	export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-
-	# Add the Cloud SDK distribution URI as a package source
-	echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-	# Import the Google Cloud Platform public key
-	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-	# Update the package list and install the Cloud SDK
-	apt-get install -y google-cloud-sdk
+	apt-get install -y
 	)
 fi
 
-apt-get install -y google-cloud-sdk-app-engine-java
+gcloud -q components update app-engine-java
 
 # Use a service account to authenticate.
 gcloud -q auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
