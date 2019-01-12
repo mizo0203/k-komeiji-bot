b9484f5bd9ffc413c42bdeef31c49e36425e5474
みぞ@CrazyBeatCoder <mizo0203@mizo0203.com>
2019-01-13 00:04:41 +0900

    Add field "node_id". fix #483 @0.75h

--- src/main/java/com/mizo0203/komeiji/repo/github/data/License.java
+++ src/main/java/com/mizo0203/komeiji/repo/github/data/License.java
@@ -9,16 +9,19 @@ public class License {
   private final String name;
   private final String spdx_id;
   private final String url;
+  private final String node_id;
 
   public License(
       @JsonProperty("key") String key,
       @JsonProperty("name") String name,
       @JsonProperty("spdx_id") String spdx_id,
-      @JsonProperty("url") String url) {
+      @JsonProperty("url") String url,
+      @JsonProperty("node_id") String node_id) {
     this.key = key;
     this.name = name;
     this.spdx_id = spdx_id;
     this.url = url;
+    this.node_id = node_id;
   }
 
   public String getKey() {
@@ -37,6 +40,10 @@ public class License {
     return url;
   }
 
+  public String getNodeId() {
+    return node_id;
+  }
+
   @Override
   public String toString() {
     return "License{"
@@ -51,6 +58,9 @@ public class License {
         + '\''
         + ", url='"
         + url
+        + '\''
+        + ", node_id='"
+        + node_id
         + '\''
         + '}';
   }
@@ -67,12 +77,13 @@ public class License {
     return Objects.equals(key, license.key)
         && Objects.equals(name, license.name)
         && Objects.equals(spdx_id, license.spdx_id)
-        && Objects.equals(url, license.url);
+        && Objects.equals(url, license.url)
+        && Objects.equals(node_id, license.node_id);
   }
 
   @Override
   public int hashCode() {
 
-    return Objects.hash(key, name, spdx_id, url);
+    return Objects.hash(key, name, spdx_id, url, node_id);
   }
 }
