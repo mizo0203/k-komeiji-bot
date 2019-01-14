cf3da3606ade505c5272c6f56b6b27f86c789860
Satoki Mizoguchi <mizo0203@mizo0203.com>
2019-01-14 12:08:32 +0900

    Add GitHub API v3 "List user repositories". fix #485 @1.0h

--- src/main/java/com/mizo0203/komeiji/HelloServlet.java
+++ src/main/java/com/mizo0203/komeiji/HelloServlet.java
@@ -1,19 +1,15 @@
 package com.mizo0203.komeiji;
 
+import com.mizo0203.komeiji.repo.GitHubClient;
 import com.mizo0203.komeiji.repo.github.data.Repository;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.logging.Level;
@@ -24,43 +20,30 @@ public class HelloServlet extends HttpServlet {
   private static final Logger LOG = Logger.getLogger(HelloServlet.class.getName());
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    Retrofit retrofit =
-        new Retrofit.Builder()
-            .baseUrl("https://api.github.com")
-            .addConverterFactory(JacksonConverterFactory.create())
-            .build();
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    GitHubClient gitHubClient = new GitHubClient(null);
 
-    GitHubService service = retrofit.create(GitHubService.class);
-
-    service
-        .listRepos("octocat")
-        .enqueue(
-            new Callback<List<Repository>>() {
-              @Override
-              public void onResponse(
-                  @Nonnull Call<List<Repository>> call,
-                  @Nonnull Response<List<Repository>> response) {
-                LOG.info("code: " + response.code());
-                LOG.info("message: " + response.message());
-                if (response.isSuccessful()) {
-                  List<Repository> repositoryList = response.body();
-                  for (Repository repository : Objects.requireNonNull(repositoryList)) {
-                    LOG.info("repository: " + repository.getName());
-                  }
-                }
+    gitHubClient.getRepositoryList(
+        "octocat",
+        new Callback<List<Repository>>() {
+          @Override
+          public void onResponse(
+              @Nonnull Call<List<Repository>> call, @Nonnull Response<List<Repository>> response) {
+            LOG.info("code: " + response.code());
+            LOG.info("message: " + response.message());
+            if (response.isSuccessful()) {
+              List<Repository> repositoryList = response.body();
+              for (Repository repository : Objects.requireNonNull(repositoryList)) {
+                LOG.info("repository: " + repository.getName());
               }
+            }
+          }
 
-              @Override
-              public void onFailure(
-                  @Nonnull Call<List<Repository>> call, @Nonnull Throwable throwable) {
-                LOG.log(Level.WARNING, "onFailure", throwable);
-              }
-            });
-  }
-
-  private interface GitHubService {
-    @GET("users/{user}/repos")
-    Call<List<Repository>> listRepos(@Path("user") String user);
+          @Override
+          public void onFailure(
+              @Nonnull Call<List<Repository>> call, @Nonnull Throwable throwable) {
+            LOG.log(Level.WARNING, "onFailure", throwable);
+          }
+        });
   }
 }
--- src/main/java/com/mizo0203/komeiji/repo/GitHubClient.java
+++ src/main/java/com/mizo0203/komeiji/repo/GitHubClient.java
@@ -3,11 +3,19 @@ package com.mizo0203.komeiji.repo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mizo0203.komeiji.repo.github.data.CreateCommitComment;
+import com.mizo0203.komeiji.repo.github.data.Repository;
 import com.mizo0203.komeiji.util.HttpUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -36,5 +44,29 @@ public class GitHubClient {
     params.put("Authorization", "token " + mAccessToken);
     String body = new ObjectMapper().writeValueAsString(createCommitComment);
     HttpUtil.post(new URL(uri), params, body, null);
+  }
+
+  /**
+   * ユーザーリポジトリの一覧表示 / List user repositories
+   *
+   * <p>指定したユーザーのパブリックリポジトリを一覧表示します。
+   *
+   * <p>List public repositories for the specified user.
+   */
+  public void getRepositoryList(String user, Callback<List<Repository>> callback) {
+    service().listRepos(user).enqueue(callback);
+  }
+
+  private GitHubService service() {
+    return new Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(JacksonConverterFactory.create())
+        .build()
+        .create(GitHubService.class);
+  }
+
+  private interface GitHubService {
+    @GET("users/{user}/repos")
+    Call<List<Repository>> listRepos(@Path("user") String user);
   }
 }
